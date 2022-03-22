479fb582e6e636ac182074b2361555f8cb12d583
Manuuzz <manu60120@gmail.com>
2022-03-22 17:12:13 +0100

    commit

--- exo2.php
+++ exo2.php
@@ -1,22 +1,24 @@
 <?php 
-    include "user.php";
+    include "personnageExo2.php";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Exo 1</title>
+    <title>Exo 2</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
 <body>
-    <?php
-        $User = new User; 
-        $User->afficheUser();
+<?php
+    
+    $perso = new Personnage();
+    $perso->_construct(); 
     
     ?>
+    
 
 
 
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+    <a href="exo1.php" >Exo1</a>
+    <a href="exo2.php" >Exo2</a>
+    <a href="exo3.php" >Exo3</a>
+
+
+
+
+
+</body>
+
+
+
+</html>
